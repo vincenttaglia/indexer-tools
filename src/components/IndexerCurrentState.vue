7ec3eeb252c46b0eca8e4e02825eece7f7ab984a
Vincent Taglia <info@vincenttaglia.com>
2022-08-03 08:27:55 -0500

    Minor bug fix for lower than 100% cut indexers

--- src/components/IndexerCurrentState.vue
+++ src/components/IndexerCurrentState.vue
@@ -254,7 +254,7 @@ export default {
           ).dp(0);
     },
     indexerCut: function(dailyRewards){
-      return this.$store.state.indexingRewardCut == 1000000 ? dailyRewards : Math.floor(dailyRewards * this.indexingRewardCut / 1000000);
+      return this.$store.state.indexingRewardCut == 1000000 ? dailyRewards : Math.floor(dailyRewards * this.$store.state.indexingRewardCut / 1000000);
     },
     updateAllocationsPerPage: function(){
       this.$cookies.set("allocations_per_page", this.allocations_per_page);
